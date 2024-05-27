338cf623d78d21412af4c7aca1905cc3a8b48517
olatzcano <116545730+olatzcano@users.noreply.github.com>
2024-05-27 11:36:41 +0200

    Add files via upload

---
+++ Angular/backend/Ms-Ciudades/src/main/java/org/zabalburu/ciudades/MsCiudadesApplication.java
@@ -0,0 +1,13 @@
+package org.zabalburu.ciudades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCiudadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCiudadesApplication.class, args);
+	}
+
+}
---
+++ Angular/backend/Ms-Ciudades/src/test/java/org/zabalburu/ciudades/MsCiudadesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zabalburu.ciudades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCiudadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
